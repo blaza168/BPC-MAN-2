eee7dd64149393584aa2668a819f4e850d568d64
blaza168 <jan.blazek10@gmail.com>
2021-10-02 15:08:50 +0200

    Export to CSV

--- entry.py
+++ entry.py
@@ -1,8 +1,12 @@
 class Entry(object):
 
-  def __init__(self, name, institution, country, publications_count, citation_count, citations_per_publication):
-    self.name = name
-    self.country = country
-    self.publications_count = publications_count
-    self.citation_count = citation_count
-    self.citations_per_publication = citations_per_publication
+    def __init__(self, name, institution, country, publications_count, citation_count):
+        self.name = name
+        self.institution = institution
+        self.country = country
+        self.publications_count = publications_count
+        self.citation_count = citation_count
+
+    def __str__(self):
+        return "%s,%s,%s,%d,%d,%.2f" % (self.name, self.institution, self.country, self.publications_count, self.citation_count,
+                                   self.publications_count / self.citation_count if self.citation_count > 0 else 0)
--- functions.py
+++ functions.py
@@ -1,11 +1,24 @@
 from entry import Entry
 import re
 
-def parse_entry(str):
-    # First , is name.
-    comma_index = str.find(",")
-    name = str[0:comma_index]
+entry_pattern = "([\w ]+),([^\d]+),([^\d]+),(\d+),(\d+)"
 
-    str = str[comma_index:]
-    print("name")
-    print(name)
+
+def parse_entry(str):
+    searchObj = re.search(entry_pattern, str, re.M | re.I)
+
+    if "Lincoln" in str:
+        print("here")
+
+    if searchObj:
+        publications = int(searchObj.group(4))
+        citations = int(searchObj.group(5))
+        return Entry(
+            searchObj.group(1),
+            searchObj.group(2),
+            searchObj.group(3),
+            publications,
+            citations
+        )
+    else:
+        raise Exception("Invalid entry! " + str)
